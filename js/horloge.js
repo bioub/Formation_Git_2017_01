1c3eee7a7517d7b95763ca1015364de6b50fa4dd
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-01-30 12:39:22 +0100

    Heures, minutes, secondes sur 2 chiffres

--- js/horloge.js
+++ js/horloge.js
@@ -3,8 +3,22 @@ var divHorloge = document.getElementById('horloge');
 function refreshHorloge() {
     var now = new Date();
     var h = now.getHours();
+
+    if (h < 10) {
+        h = '0' + h;
+    }
+
     var m = now.getMinutes();
+
+    if (m < 10) {
+        m = '0' + m;
+    }
+
     var s = now.getSeconds();
+
+    if (s < 10) {
+        s = '0' + s;
+    }
 
     var format = h+'h'+m+'m'+s;
 
